0329704834590bc643234beb5c57cf6ed4c4588a
ralph-12 <tmdrl1026@naver.com>
2023-05-30 18:48:24 +0900

    add home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        elevation: 0,
+        title: const Text(
+          '오늘의 웹툰',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+}
